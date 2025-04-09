80d5f563f0583f629d2c6de90463870f7f6ee275
Fjoggs <vegard@fjobeno.com>
2025-04-09 22:27:39 +0200

    Remove arrow as I never use it

--- dot_config/nvim/lua/fjogen/plugins/colorscheme.lua
+++ dot_config/nvim/lua/fjogen/plugins/colorscheme.lua
@@ -1,41 +1,9 @@
--- return {
--- 	"folke/tokyonight.nvim",
--- 	priority = 1000,
--- 	config = function()
--- 		vim.cmd("colorscheme tokyonight")
--- 		-- vim.cmd("colorscheme tokyonight-day")
--- 	end,
--- }
--- return {
--- 	"marko-cerovac/material.nvim",
--- 	priority = 1000,
--- 	config = function()
--- 		vim.g.material_style = "oceanic"
--- 		vim.cmd("colorscheme material")
--- 	end,
--- }
---
--- return {
--- 	"sainnhe/edge",
--- 	priority = 1000,
--- 	config = function()
--- 		vim.cmd("colorscheme edge")
--- 	end,
--- }
--- lua/plugins/rose-pine.lua
--- return {
--- 	"rose-pine/neovim",
--- 	name = "rose-pine",
--- 	config = function()
--- 		vim.cmd("colorscheme rose-pine-dawn")
--- 	end,
--- }
 return {
 	dependencies = {
-		"hrsh7th/nvim-cmp",
 		"sainnhe/edge",
 		"marko-cerovac/material.nvim",
-		"navarasu/onedark.nvim",
+		"rose-pine/neovim",
+		"folke/tokyonight.nvim",
 	},
 	"navarasu/onedark.nvim",
 	priority = 1000,
